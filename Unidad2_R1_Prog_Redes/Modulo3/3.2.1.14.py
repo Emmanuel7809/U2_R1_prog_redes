eae04a79c7f183e984940aaf9827c954601096ec
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-23 15:04:59 -0600

    Update 3.2.1.14.py

--- Unidad2_R1_Prog_Redes/Modulo3/3.2.1.14.py
+++ Unidad2_R1_Prog_Redes/Modulo3/3.2.1.14.py
@@ -2,7 +2,6 @@
 #Recuperacion 1 Unidad 2 Introducción a las REST API
 #Programación de Redes 
 #Profesor: Gabriel Barron Rodriguez
-
 class Stack:
     def __init__(self):
         self.__stk = []
@@ -15,25 +14,24 @@ class Stack:
         del self.__stk[-1]
         return val
 
-
 class CountingStack(Stack):
     def __init__(self):
-        # Llena el constructor con acciones apropiadas.
-        super().__init__()  
-        self.__counter = 0  
+        super().__init__()
+        self.__counter = 0
 
     def get_counter(self):
-        # Presenta el valor actual del contador al mundo.
         return self.__counter
 
     def pop(self):
-        # Haz un pop y actualiza el contador.
-        val = super().pop()  
-        self.__counter += 1  
+        val = super().pop()
+        self.__counter += 1
         return val
 
-stk = CountingStack()
-for i in range(100):
-    stk.push(i)
-    stk.pop()
-print(stk.get_counter())
+if __name__ == "__main__":
+    stk = CountingStack()
+    
+    for i in range(100):
+        stk.push(i)
+        stk.pop()
+    
+    print(stk.get_counter())
