6d174f5cf68791af2a36df219f641c766b9f35e7
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-23 15:14:21 -0600

    Update 3.4.1.14.py

--- Unidad2_R1_Prog_Redes/Modulo3/3.4.1.14.py
+++ Unidad2_R1_Prog_Redes/Modulo3/3.4.1.14.py
@@ -2,7 +2,6 @@
 #Recuperacion 1 Unidad 2 Introducción a las REST API
 #Programación de Redes 
 #Profesor: Gabriel Barron 
-
 import math
 
 class Point:
@@ -22,7 +21,10 @@ class Point:
     def distance_from_point(self, point):
         return math.hypot(self.__x - point.getx(), self.__y - point.gety())
 
-point1 = Point(0, 0)
-point2 = Point(1, 1)
-print(point1.distance_from_point(point2))
-print(point2.distance_from_xy(2, 0))
+if __name__ == "__main__":
+    point1 = Point(0, 0)
+    point2 = Point(1, 1)
+    
+    print(point1.distance_from_point(point2))
+    print(point2.distance_from_xy(2, 0))
+
