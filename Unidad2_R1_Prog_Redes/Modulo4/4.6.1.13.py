f162c0754d8a4c7ae4ce675e9f3a10d80141a03e
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-23 16:38:08 -0600

    Update 4.6.1.13.py

--- Unidad2_R1_Prog_Redes/Modulo4/4.6.1.13.py
+++ Unidad2_R1_Prog_Redes/Modulo4/4.6.1.13.py
@@ -2,34 +2,23 @@
 #Recuperacion 1 Unidad 2 Introducción a las REST API
 #Programación de Redes 
 #Profesor: Gabriel Barron Rodriguez
+from calendar import Calendar
 
-from calendar import Calendar, SATURDAY, SUNDAY
-
-class MyCalendar(Calendar):
-    def count_weekday_in_year(self, year, weekday):
-        """
-        Counts the number of occurrences of a weekday in a year.
-        
-        Parameters:
-            year (int): The year to check.
-            weekday (int): The day of the week to count (0 = Monday, 1 = Tuesday, ..., 6 = Sunday).
-        
-        Returns:
-            int: The number of occurrences of the specified weekday in the year.
-        """
+class MiCalendario(Calendar):
+    def contar_dia_de_la_semana_en_año(self, year, weekday):
         if not (0 <= weekday <= 6):
-            raise ValueError("weekday must be a number between 0 and 6.")
+            raise ValueError("El día de la semana debe ser un número entre 0 y 6.")
         
         count = 0
         for month in range(1, 13):
-            _, weekdays = self.monthdays2calendar(year, month)
-            for day in weekdays:
-                if day[weekday] != 0:
-                    count += 1
+            for week in self.monthdayscalendar(year, month):
+                for day in week:
+                    if day == weekday and day != 0:
+                        count += 1
         
         return count
 
-calendar = MyCalendar()
+mi_calendario = MiCalendario()
 
-print(calendar.count_weekday_in_year(2019, 0)) # 52
-print(calendar.count_weekday_in_year(2000, 6)) # 53
+print(mi_calendario.contar_dia_de_la_semana_en_año(2019, 0))  # Número de veces que ocurrió el lunes en 2019
+print(mi_calendario.contar_dia_de_la_semana_en_año(2000, 6))  # Número de veces que ocurrió el domingo en 2000
