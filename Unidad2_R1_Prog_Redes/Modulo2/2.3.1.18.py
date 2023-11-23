78a6ae50db6e2a96d5525d2200ab2cbe41b24e4e
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-23 14:51:59 -0600

    Update 2.3.1.18.py

--- Unidad2_R1_Prog_Redes/Modulo2/2.3.1.18.py
+++ Unidad2_R1_Prog_Redes/Modulo2/2.3.1.18.py
@@ -2,12 +2,8 @@
 #Recuperacion 1 Unidad 2 Introducción a las REST API
 #Programación de Redes 
 #Profesor: Gabriel Barron Rodriguez
-
-
 def mysplit(strng):
-    words = strng.split()
-
-    return words
+    return strng.split()
 
 print(mysplit("Ser o no ser, esa es la pregunta"))
 print(mysplit("Ser o no ser,esa es la pregunta"))
