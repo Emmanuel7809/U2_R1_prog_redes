7f67f696abfc545c3aae5a29b7af8c0e4b93fb50
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-23 15:06:50 -0600

    Update 3.2.1.15.py

--- Unidad2_R1_Prog_Redes/Modulo3/3.2.1.15.py
+++ Unidad2_R1_Prog_Redes/Modulo3/3.2.1.15.py
@@ -2,32 +2,26 @@
 #Recuperacion 1 Unidad 2 Introducción a las REST API
 #Programación de Redes 
 #Profesor: Gabriel Barron Rodriguez
-
 class QueueError(IndexError):
     pass
-
 
 class Queue:
     def __init__(self):
         self.items = []
-
     def put(self, elem):
         self.items.insert(0, elem)
-
     def get(self):
         if not self.items:
             raise QueueError("La cola está vacía")
         return self.items.pop()
-
-
-que = Queue()
-que.put(1)
-que.put("perro")
-que.put(False)
-
-try:
-    for i in range(4):
-        print(que.get())
-except QueueError as e:
-    print(f"Error de Cola: {e}")
+if __name__ == "__main__":
+    que = Queue()
+    que.put(1)
+    que.put("perro")
+    que.put(False)
+    try:
+        for i in range(4):
+            print(que.get())
+    except QueueError as e:
+        print(f"Error de Cola: {e}")
 
