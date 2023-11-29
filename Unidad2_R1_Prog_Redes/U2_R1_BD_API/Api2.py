34f6a46b2c1e2307825d44569ed76150cba23ad0
Emmanuel7809 <144484672+Emmanuel7809@users.noreply.github.com>
2023-11-29 11:43:25 -0600

    Update Api2.py

--- Unidad2_R1_Prog_Redes/U2_R1_BD_API/Api2.py
+++ Unidad2_R1_Prog_Redes/U2_R1_BD_API/Api2.py
@@ -1,8 +1,26 @@
+###Alan Francisco Emmanuel Aguilar Fuentes
+### Programacion de Redes
+### Api 2 traductor 
 import http.client
 
+# Clave de API para acceder al servicio de traducción
 API_KEY = "0ceb9beaf7msh48113a6f6b7e45ep1b530bjsn77587cdf5efc"
 
 def translate_text(source_language, target_language, text):
+    """
+    Traduce un texto desde un idioma de origen a un idioma de destino utilizando un servicio de traducción.
+
+    Args:
+        source_language (str): Idioma de origen del texto.
+        target_language (str): Idioma de destino al cual se desea traducir el texto.
+        text (str): Texto que se va a traducir.
+
+    Returns:
+        str: El resultado de la traducción.
+
+    Raises:
+        Exception: Si hay algún problema con la solicitud de traducción.
+    """
     conn = http.client.HTTPSConnection("text-translator2.p.rapidapi.com")
     payload = f"source_language={source_language}&target_language={target_language}&text={text}"
     headers = {
@@ -13,9 +31,20 @@ def translate_text(source_language, target_language, text):
 
     conn.request("POST", "/translate", payload, headers)
     res = conn.getresponse()
+
+    if res.status != 200:
+        raise Exception(f"Error en la solicitud de traducción. Código de estado: {res.status}")
+
     return res.read().decode("utf-8")
 
 def main():
+    """
+    Función principal que solicita al usuario información necesaria para la traducción
+    y muestra el resultado de la traducción. Permite al usuario salir del programa.
+
+    Returns:
+        None
+    """
     while True:
         source_language = input("Idioma de origen: ")
         target_language = input("Idioma de destino: ")
@@ -25,12 +54,14 @@ def main():
             print("Por favor, ingrese valores válidos.")
             continue
 
-        result = translate_text(source_language, target_language, text)
-        print("Resultado de la traducción:", result)
+        try:
+            result = translate_text(source_language, target_language, text)
+            print("Resultado de la traducción:", result)
+        except Exception as e:
+            print(f"Error: {e}")
 
         if input("¿Desea salir? (s/n): ").lower() == 's':
             break
 
 if __name__ == "__main__":
     main()
-
